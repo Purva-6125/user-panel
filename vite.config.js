490fcc61d37b6f54063ca01fa3b40103702c62a8
Purva <purvabakare04@gmail.com>
2025-03-26 20:04:51 +0530

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Allows external access
-    port: process.env.PORT || 5173, // Use Render's assigned port
-  },
-});
+})
