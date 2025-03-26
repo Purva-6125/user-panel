a632932a810f042eb541458aa2ca43ee3bddd881
Purva <purvabakare04@gmail.com>
2025-03-26 19:50:46 +0530

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // 🔹 Yeh line add karein
+    port: 5173, // 🔹 Apna desired port likhein
+  },
+});
